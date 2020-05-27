9e4cdf43541f5c2126fef115b3a0b5001a789f25
Juan <juan.ahumada94@gmail.com>
2020-05-27 12:55:22 -0500

    splash screen activity

--- app/src/main/java/com/itnl/expotech/view/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/itnl/expotech/view/ui/activities/SplashScreenActivity.kt
@@ -1,8 +1,12 @@
 package com.itnl.expotech.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.itnl.expotech.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -10,5 +14,25 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val animacion = AnimationUtils.loadAnimation(this, R.anim.animacion)
+        ivLogoSplash.startAnimation(animacion)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        animacion.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+        })
     }
 }
